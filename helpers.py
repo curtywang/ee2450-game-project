2d8225a966c673d520bd000fca22f71aa4c6824f
Curtis Wang <ywang11@calstatela.edu>
2020-12-21 14:42:00 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -48,8 +48,8 @@ def load_bgmusic(filename: str, filedir: str = 'assets/snd/') -> None:
     """
     Loads a sound file into the mixer
 
-    :param filename: string of the image file to load (such as 'lander.jpg')
-    :param filedir: optional string of the folder to pull from, defaults to 'assets/img/'
+    :param filename: string of the sound file to load (such as 'lander.mp4')
+    :param filedir: optional string of the folder to pull from, defaults to 'assets/snd/'
     """
     if not pg.mixer:
         raise ValueError('Pygame sound mixer not installed properly')
