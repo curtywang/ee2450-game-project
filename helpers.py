3fb44d0ded951c070c403cd9699f9d0422ef86a7
Curtis Wang <ywang11@calstatela.edu>
2020-12-21 14:41:31 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -30,8 +30,8 @@ def load_sound(filename: str, filedir: str = 'assets/snd/') -> pg.mixer.Sound:
     """
     Loads a sound file and returns the converted pygame Sound object
 
-    :param filename: string of the image file to load (such as 'lander.jpg')
-    :param filedir: optional string of the folder to pull from, defaults to 'assets/img/'
+    :param filename: string of the sound file to load (such as 'music.mp4')
+    :param filedir: optional string of the folder to pull from, defaults to 'assets/snd/'
     :return:
     """
     if not pg.mixer:
